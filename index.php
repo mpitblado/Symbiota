aefa5c9db3336f04558a723a0c69391f26f61e2f
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-10-14 07:11:23 -0700

    remove logos to put them in footer

--- index.php
+++ index.php
@@ -144,11 +144,9 @@ header("Content-Type: text/html; charset=".$CHARSET);
 				<img src="<?php echo $CLIENT_ROOT; ?>/images/iraf.jpeg" style="height:50px; padding: 10px" />
 				<img src="<?php echo $CLIENT_ROOT; ?>/images/IRET_logo.jpg" style="height:50px; padding: 10px" />
 				<a href="https://nationalzoo.si.edu/ccs/gabon-program" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/Smithsonian_logo.jpg" style="height:50px; padding: 10px" /></a>
-				<a href="https://www.nature.org/en-us/about-us/where-we-work/africa/gabon/" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/TNC_logo.png" style="height:50px; padding: 10px" /></a>
 				<a href="https://univ-masuku.org/" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/USTM_logo.png" style="height:50px; padding: 10px" /></a>
 				<a href="https://biokic.asu.edu/" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/ASU_biokic_logo.png" style="height:50px; padding: 10px" /></a>
 				<a href="https://www.floridamuseum.ufl.edu/nhdept/" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/FM_logo.png" style="height:50px; padding: 10px" /></a>
-				<a href="https://www.gbif.org/" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/GBIF_logo.png" style="height:50px; padding: 10px" /></a>
 		</div>
 	</div>
 	<?php
