acf0b2638ccca73c99af239f8f638470d82c55a9
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-10-14 07:20:54 -0700

    fix footer spacing and logo sizes

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 	</tr>
 	<tr>
 		<td id="footer"  colspan="3">
-			<div>
+			<div style="text-align:center;margin-top:15px">
 			<?php
 				if($LANG_TAG=='en'){
 			?>
@@ -21,10 +21,9 @@
 			</div>
 			<p>
 				<a href="https://www.gbif.org/" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/GBIF_logo.png" style="height:50px; padding: 10px" /></a>
-				<a href="https://www.nature.org/en-us/about-us/where-we-work/africa/gabon/" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/TNC_logo.png" style="height:50px; padding: 10px" /></a>
+				<a href="https://www.nature.org/en-us/about-us/where-we-work/africa/gabon/" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/TNC_logo.png" style="width:150px; padding: 10px" /></a>
 				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="height:50px; padding: 10 px" /></a>
 			</p>
-			<div style="text-align:center;margin-bottom:15px">This project made possible by GBIF Award: <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=" target="_blank">#------</a></div>				
  		</td>
 	</tr>
 </table>
