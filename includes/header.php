93f8c8ebfe8e1de6c3c676c53f5e0356c710991f
Edward Gilbert <egbot@asu.edu>
2022-02-01 22:34:06 -0700

    Header Menu update
    - Added reference to GBIF project

--- includes/header.php
+++ includes/header.php
@@ -144,7 +144,11 @@ else include_once($SERVER_ROOT.'/content/lang/header.'.$LANG_TAG.'.php');
 								<a href="<?php echo $CLIENT_ROOT; ?>/collections/misc/collprofiles.php" ><?php echo (isset($LANG['H_PARTNERS'])?$LANG['H_PARTNERS']:'Partners'); ?></a>
 							</li>
 							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/misc/TDWG_2021/index.php" ><?php echo (isset($LANG['H_TDWG_POSTER'])?$LANG['H_TDWG_POSTER']:'TDWG-2021 Poster'); ?></a>
+								<a href="https://www.gbif.org/project/BID-AF2020-122-NAC/the-gabon-biodiversity-portal" target="_blank">GBIF BID-AF2020-122-NAC</a>
+							</li>
+
+							<li>
+								<a href="<?php echo $CLIENT_ROOT; ?>/misc/TDWG_2021/index.php" target="_blank"><?php echo (isset($LANG['H_TDWG_POSTER'])?$LANG['H_TDWG_POSTER']:'TDWG-2021 Poster'); ?></a>
 							</li>
 							<!--
 							<li>
