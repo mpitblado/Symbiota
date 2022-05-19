12d6118194172aacccc82e53b4c5fb78df448127
Greg Post <gregory.post@asu.edu>
2022-05-19 07:22:20 -0700

    Basic agent add, edit, and list working
    Initial commit for agent functionality. Add, Edit, and list is working with a few agent fields.

--- agents/index.php
+++ agents/index.php
@@ -1,134 +1,207 @@
 <?php
-include_once('../config/symbini.php');
-include_once($SERVER_ROOT.'/classes/AgentManager.php');
+include_once ('../config/symbini.php');
+include_once ($SERVER_ROOT . '/classes/AgentManager.php');
+header("Content-Type: text/html; charset=".$CHARSET);
 
-Header("Content-Type: text/html; charset=".$CHARSET);
+$agentID = array_key_exists('agentID', $_REQUEST) ? $_REQUEST['agentID'] : '';
+$submitAction = array_key_exists('submitaction', $_POST) ? $_POST['submitaction'] : '';
 
-$lang = array_key_exists("lang",$_REQUEST)?$_REQUEST["lang"]:$defaultLang;
+// Sanitation
+if(!is_numeric($agentID)) $agentID = 0;
+$submitAction = filter_var($submitAction, FILTER_SANITIZE_STRING);
 
-$spDisplay = "Agent Search";
+$agentManager = new AgentManager();
 
-$name = array_key_exists("name",$_REQUEST)?$_REQUEST["name"]:"";
+$isEditor = false;
+if($IS_ADMIN || array_key_exists('CollAdmin',$USER_RIGHTS)) $isEditor = true;
 
-pageheader($name);
-if (strlen($name)>0) {
-   echo searchform($name);
-   echo search($name);
-} else {
-   echo searchform();
-   echo browse();
-}
-footer();
-
-function search($term) {
-  $am = new AgentManager();
-  $result  =  "<div id='loadedWithPage'><table id='innertable'>\n";
-  $result .= $am->agentNameSearch($term);
-  $result .= "</table></div>\n";
-  return $result;
-}
-
-function browse() {
-  $am = new AgentManager();
-  $result  =  "<div id='loadedWithPage'>\n";
-  $result .= "<h3>Collector and other agent records</h3>";
-  $result .= $am->getNameStats();
-  $result .= "<ul><li>Individuals</li>";
-  $result .= $am->getLastNameLinks();
-  $result .= "</ul><ul><li>Teams</li>";
-  $result .= $am->getTeamLinks();
-  $result .= "</ul><ul><li>Organizations</li>";
-  $result .= $am->getOrganizationLinks();
-  $result .= "</ul>";
-  $result .= "</div>\n";
-  return $result;
+$statusStr = '';
+if($isEditor && $submitAction) {
+	if($submitAction == 'submitAgentEdits'){
+		$status = $agentManager->editAgent($_POST);
+		if(!$status) $statusStr = $agentManager->getErrorMessage();
+	}
+	elseif($submitAction == 'deleteAgent'){
+		$status = $agentManager->deleteAgent($_POST['delAgentID']);
+		if(!$status) $statusStr = $agentManager->getErrorMessage();
+	}
+		elseif($submitAction == 'addAgent'){
+		$status = $agentManager->addAgent($_POST);
+		if(!$status) $statusStr = $agentManager->getErrorMessage();
+	}
 }
 
-function searchform($name="") {
-   global $CLIENT_ROOT;
-   $result  = "<div id='formDiv'>";
-   $result .= "<form method='GET' id='queryForm' style='display:inline;' >\n";
-   $result .= "<input type='text' name='name' value='$name'>";
-   $result .= "<input type='submit'>";
-   $result .= "</form>\n";
-   $result .= "<span id='plinkSpan'></span></div>\n";
-   $result .= '<script type="text/javascript">
-   var frm = $("#queryForm");
-   frm.submit(function(event) {
-      $("#statusDiv").html("Searching...");
-      $.ajax({
-         url: "'.$CLIENT_ROOT.'/agents/rpc/handler.php",
-         data: frm.serialize(),
-         type: "GET",
-         dataType : "html",
-         success: function( data ) {
-            $("#responseDiv").html(data);
-            $("#loadedWithPage").html("");
-            var permalink = "&nbsp;<a href=\''.$CLIENT_ROOT.'/agents/index.php?" + frm.serialize() +"\'>Permalink</a>";
-            $("#plinkSpan").html(permalink);
-         },
-         error: function( xhr, status, errorThrown ) {
-            $("#statusDiv").html("Error. " + errorThrown);
-            console.log( "Error: " + errorThrown );
-            console.log( "Status: " + status );
-            console.dir( xhr );
-         },
-         complete: function( xhr, status ) {
-             $("#statusDiv").html("");
-         }
-      });
-      event.preventDefault();
-   });
-</script>
-';
-   $result .= "<div id='responseDiv'></div>\n";
-   $result .= "<div id='statusDiv'></div>\n";
-
-   return $result;
-}
-
-function pageheader($name) {
-   global $SERVER_ROOT, $DEFAULT_TITLE, $spDisplay, $CLIENT_ROOT, $agents_indexMenu, $agents_indexCrumbs;
-echo '<!DOCTYPE HTML>
-<html>
-<head>
-	<title>'.$DEFAULT_TITLE.' - '.$spDisplay. '</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $CHARSET; ?>"/>
-	<meta name="keywords" content='. $spDisplay .' />
-	<link href="../css/base.css?ver=<?php echo $CSS_VERSION; ?>" type="text/css" rel="stylesheet" />
-	<link href="../css/main.css'.(isset($CSS_VERSION_LOCAL)?'?ver='.$CSS_VERSION_LOCAL:'').'" type="text/css" rel="stylesheet" />
-	<script type="text/javascript" src="../js/jquery.js"></script>
-	<script type="text/javascript" src="../js/jquery-ui.js"></script>';
-    // include_once($SERVER_ROOT.'/includes/googleanalytics.php');
-echo '<script type="text/javascript">
-		var currentLevel = ' . ($descrDisplayLevel?$descrDisplayLevel:"1"). ';
-		var levelArr = new Array('. ($descr?"'".implode("','",array_keys($descr))."'":"") . ');
-	</script>
-</head>
-<body>';
-   $displayLeftMenu = (isset($agents_indexMenu)?$agents_indexMenu:false);
-   include($SERVER_ROOT.'/includes/header.php');
-   if(!isset($agent_indexCrumbs)){
-      $agent_indexCrumbs = array();
-      array_push($agent_indexCrumbs,"<a href='$CLIENT_ROOT/index.php'>Home</a>");
-      array_push($agent_indexCrumbs,"<a href='$CLIENT_ROOT/agents/index.php'>Agents</a>");
-   }
-   if (strlen($name)>0) {
-      array_push($agent_indexCrumbs,"<a href='$CLIENT_ROOT/agents/index.php?name=$name'>Search</a>");
-   }
-   echo "<div class='navpath'>";
-   $last = array_pop($agent_indexCrumbs);
-   echo implode($agent_indexCrumbs, " &gt;&gt;");
-   echo " &gt;&gt;<strong>$last</strong>";
-   array_push($agent_indexCrumbs,$last);
-   echo "</div>";
-}
-
-
-function footer() {
-   global $SERVER_ROOT,$CLIENT_ROOT;
-  include($SERVER_ROOT.'/includes/footer.php');
-  echo "</body>\n</html>";
-}
+$agentsArr = $agentManager->getAgentList();
 
 ?>
+<html>
+<head>
+	<title><?php echo $DEFAULT_TITLE; ?> - Agent Manager</title>
+	<?php
+	$activateJQuery = true;
+	include_once ($SERVER_ROOT.'/includes/head.php');
+	?>
+	<script src="<?php echo $CLIENT_ROOT; ?>/js/jquery.js" type="text/javascript"></script>
+	<script src="<?php echo $CLIENT_ROOT; ?>/js/jquery-ui.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		function toggleEditor(){
+			$(".editTerm").toggle();
+			$(".editFormElem").toggle();
+			$("#editButton-div").toggle();
+			$("#edit-legend").toggle();
+			$("#unitDel-div").toggle();
+		}
+	</script>
+	<style type="text/css">
+		fieldset{ margin: 10px; padding: 15px; width: 800px }
+		legend{ font-weight: bold; }
+		label{ text-decoration: underline; }
+		#edit-legend{ display: none }
+		.field-div{ margin: 3px 0px }
+		.editIcon{  }
+		.editTerm{ }
+		.editFormElem{ display: none }
+		#editButton-div{ display: none }
+		#unitDel-div{ display: none }
+		.button-div{ margin: 15px }
+		.link-div{ margin:0px 30px }
+		#status-div{ margin:15px; padding: 15px; color: red; }
+	</style>
+</head>
+<body>
+	<?php
+	$displayLeftMenu = (isset($profile_indexMenu)?$profile_indexMenu:'true');
+	include($SERVER_ROOT.'/includes/header.php');
+	?>
+	<div class="navpath">
+		<a href="../index.php">Home</a> &gt;&gt;
+		<b><a href="index.php">Agent List</a></b>
+	</div>
+	<div id='innertext'>
+		<?php
+		if($statusStr){
+			echo '<div id="status-div">'.$statusStr.'</div>';
+		}
+		if($agentID){
+			$Agent = $agentManager->getAgent($agentID);
+			?>
+			<div id="updateAgent-div" style="clear:both;margin-bottom:10px;">
+				<fieldset id="edit-fieldset">
+					<legend>Edit <span id="edit-legend"> Agent</span></legend>
+					<div style="float:right">
+						<span class="editIcon"><a href="#" onclick="toggleEditor()"><img class="editimg" src="../images/edit.png" /></a></span>
+
+					</div>
+					<form name="agentEditForm" action
+               ="index.php" method="post">
+						<div class="field-div">
+							<label>First Name</label>:
+							<span class="editTerm"><?php echo $Agent['firstName']; ?></span>
+							<span class="editFormElem"><input type="text" name="firstName" value="<?php echo $Agent['firstName'] ?>" style="width:200px;" required /></span>
+						</div>
+						<div class="field-div">
+							<label>Family Name</label>:
+							<span class="editTerm"><?php echo $Agent['familyName']; ?></span>
+							<span class="editFormElem"><input type="text" name="familyName" value="<?php echo $Agent['familyName'] ?>" style="width:50px;" required /></span>
+						</div>
+						<div class="field-div">
+							<label>Middle Name</label>:
+							<span class="editTerm"><?php echo $Agent['middleName']; ?></span>
+							<span class="editFormElem"><input type="text" name="middleName" value="<?php echo $Agent['middleName'] ?>" style="width:50px;" /></span>
+						</div>
+						<div class="field-div">
+							<label>Active Start Year</label>:
+							<span class="editTerm"><?php echo $Agent['startYearActive']; ?></span>
+							<span class="editFormElem"><input type="text" name="startYearActive" value="<?php echo $Agent['startYearActive'] ?>"style="width:50px;" /></span>
+						</div>
+						<div class="field-div">
+							<label>Active End Year</label>:
+							<span class="editTerm"><?php echo $Agent['endYearActive']; ?></span>
+							<span class="editFormElem"><input type="text" name="endYearActive" value="<?php echo $Agent['endYearActive'] ?>" style="width:50px;" /></span>
+						</div>
+						<div class="field-div">
+							<label>Notes</label>:
+							<span class="editTerm"><?php echo $Agent['notes']; ?></span>
+							<span class="editFormElem"><input type="text" name="notes" value="<?php echo $Agent['notes'] ?>" maxlength="250" style="width:650px;" /></span>
+						</div>
+						<div id="editButton-div" class="button-div">
+							<input name="agentID" type="hidden" value="<?php echo $agentID; ?>" />
+							<button type="submit" name="submitaction" value="submitAgentEdits">Save Edits</button>
+						</div>
+					</form>
+				</fieldset>
+			</div>
+			<div id="agentDel-div">
+				<form name="agentDeleteForm" action="index.php" method="post">
+					<fieldset>
+						<legend>Delete Agemt</legend>
+						<div class="button-div">
+							<input name="delAgentID" type="hidden"  value="<?php echo $agentID; ?>" />
+							<button type="submit" name="submitaction" value="deleteAgent" onclick="return confirm('Are you sure you want to delete this record?')">Delete Agent</button>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+			<?php
+		}
+		else{
+			?>
+			<div style="float:right">
+				<span class="editIcon"><a href="#" onclick="$('#addAgent-div').toggle();"><img class="editimg" src="../images/add.png" /></a></span>
+			</div>
+			<div id="addAgent-div" style="clear:both;margin-bottom:10px;display:none">
+				<!--This should also be visible when !$agentID -->
+				<fieldset id="new-fieldset">
+					<legend>Add Agent</legend>
+					<form name="agentAddForm" action="index.php" method="post">
+						<div class="field-div">
+							<label>First Name</label>:
+							<span><input type="text" name="firstName" style="width:200px;" required /></span>
+						</div>
+						<div class="field-div">
+							<label>Family Name</label>:
+							<span><input type="text" name="familyName" style="width:200px;" required /></span>
+						</div>
+						<div class="field-div">
+							<label>Middle Name</label>:
+							<span><input type="text" name="middleName" style="width:50px;" /></span>
+						</div>
+						<div class="field-div">
+							<label>Active Start Year</label>:
+							<span><input type="text" name="startYearActive" style="width:50px;" /></span>
+						</div>
+						<div class="field-div">
+							<label>Active End Year</label>:
+							<span><input type="text" name="endYearActive" style="width:50px;" /></span>
+						</div>
+						<div class="field-div">
+							<label>Notes</label>:
+							<span><input type="text" name="notes" maxlength="250" style="width:200px;" /></span>
+						</div>
+						<div id="addButton-div" class="button-div">
+							<button type="submit" name="submitaction" value="addAgent">Add Agent</button>
+						</div>
+					</form>
+				</fieldset>
+			</div>
+			<?php
+			if($agentsArr){
+				$titleStr = '';
+				echo '<div style=";font-size:1.3em;margin: 10px 0px">'.$titleStr.'</div>';
+				echo '<ul>';
+				foreach($agentsArr as $agentID => $agentArr){
+					$termDisplay = '<a href="index.php?agentID='.$agentID.'">'.$agentArr['familyName'].','.$agentArr['firstName'].'</a>';
+					echo '<li>'.$termDisplay.'</li>';
+				}
+				echo '</ul>';
+			}
+			else echo '<div>No records returned</div>';
+			if($agentID) echo '<div class="link-div"><a href="index.php">Show Agent list</a></div>';
+		}
+		?>
+	</div>
+	<?php
+	include($SERVER_ROOT.'/includes/footer.php');
+	?>
+</body>
+</html>
