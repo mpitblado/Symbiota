1ccf64c4d0a43fa47861fa5147d1e8dc40b45406
Laura Rocha Prado <lauraprado@asu.edu>
2023-05-05 17:29:10 -0700

    Reworks exporter to filter children by rankid range
    - Allows for splitting symbiota csv files per kingdom

--- classes/TaxonomyExporter.php
+++ classes/TaxonomyExporter.php
@@ -30,7 +30,7 @@ class TaxonomyExporter extends Manager
 
 	/** Defines the root node of the tree
 	 * @param $node - the node to be used as the root (integer, default = 0 for full portal taxonomy; or tid of a taxon)
-	 * @return array - the root node
+	 * @return array - the root node(s)
 	 */
 	function setRootNode($node)
 	{
@@ -61,7 +61,7 @@ class TaxonomyExporter extends Manager
 			$rootNodeArr[] = array("tid" => $tid, "sciname" => $sciname, "author" => $author, "rankid" => $rankid);
 		}
 		$stmt->close();
-		$rootNode = $rootNodeArr[0];
+		$rootNode = $rootNodeArr;
 		return $rootNode;
 	}
 
@@ -80,25 +80,51 @@ class TaxonomyExporter extends Manager
 		$stmt->bind_result($taxonID, $kingdom, $family, $sciname, $author, $genus, $specificepithet, $taxonrank, $infraspecificepithet, $rankid, $rankname, $source, $parentstr);
 		$nodeDataArr = array();
 		while ($stmt->fetch()) {
-			$nodeDataArr[] = array("taxonID" => $taxonID, "kingdom" => $kingdom, "family" => $family, "sciname" => $sciname, "author" => $author, "genus" => $genus, "specificepithet" => $specificepithet, "taxonrank" => $taxonrank, "infraspecificepithet" => $infraspecificepithet, "rankid" => $rankid, "rankname" => $rankname, "source" => $source, "parentstr" => $parentstr);
+			$nodeDataArr = array("taxonID" => $taxonID, "kingdom" => $kingdom, "family" => $family, "sciname" => $sciname, "author" => $author, "genus" => $genus, "specificepithet" => $specificepithet, "taxonrank" => $taxonrank, "infraspecificepithet" => $infraspecificepithet, "rankid" => $rankid, "rankname" => $rankname, "source" => $source, "parentstr" => $parentstr);
 		}
 		$stmt->close();
 		return $nodeDataArr;
 	}
 
+	/**
+	 * Gets the immediate children of a node
+	 * @param node - the tid of the node to get the children of
+	 * @param rankid - the rankid of the node to get the children of
+	 * @return array - the children of the node
+	 */
+	function getNextChildren($node, $rankid)
+	{
+		$stmt = "SELECT DISTINCT t.tid AS taxonID, t.kingdomName AS kingdom, ts.family, t.sciname, t.author, CONCAT_WS(' ', t.unitind1, t.unitname1) AS genus, CONCAT_WS(' ', t.unitind2, t.unitname2) AS specificepithet, t.unitind3 AS taxonrank, t.unitname3 AS infraspecificepithet, t.rankid, tu.rankname, t.source, p.sciname AS parentstr FROM taxa t INNER JOIN taxstatus ts ON t.tid = ts.tid  INNER JOIN taxaenumtree e ON ts.tid = e.tid AND ts.taxauthid = e.taxauthid INNER JOIN taxa p ON ts.parentTid = p.tid INNER JOIN taxa a ON ts.tidaccepted = a.tid LEFT JOIN taxonunits tu ON t.rankid = tu.rankid  WHERE ts.taxauthid = 1 AND e.parentTid = ? AND t.rankid = (SELECT MIN(t.rankid) FROM taxaenumtree e INNER JOIN taxa t ON e.tid = t.tid WHERE taxauthid = 1 AND e.parenttid = ? AND t.rankid > ?);";
+
+		$stmt = $this->conn->prepare($stmt);
+		$stmt->bind_param('iii', $node, $node, $rankid);
+		$stmt->execute();
+
+		$stmt->bind_result($taxonID, $kingdom, $family, $sciname, $author, $genus, $specificepithet, $taxonrank, $infraspecificepithet, $rankid, $rankname, $source, $parentstr);
+
+		$nodeNextChildrenArr = array();
+		while ($stmt->fetch()) {
+			$nodeNextChildrenArr[] = array("taxonID" => $taxonID, "kingdom" => $kingdom, "family" => $family, "sciname" => $sciname, "author" => $author, "genus" => $genus, "specificepithet" => $specificepithet, "taxonrank" => $taxonrank, "infraspecificepithet" => $infraspecificepithet, "rankid" => $rankid, "rankname" => $rankname, "source" => $source, "parentstr" => $parentstr);
+		}
+		$stmt->close();
+		return $nodeNextChildrenArr;
+	}
+
 	/** Gets the children of a node
 	 * @param $node - the tid to get the children of (the root tid of the tree)
+	 * @param $minRankId - the minimum rankid to be included in the returned array default = 1 for full portal taxonomy
+	 * @param $maxRankId - the maximum rankid to be included in the returned array default = 
 	 * @return array - the children of the node
 	 * todo: add a parameter to limit the number of children returned?
 	 * todo: add more fields to the returned array
 	 */
-	function getNodeChildren($node)
+	function getNodeChildren($node, $minRankId = 1, $maxRankId = 500)
 	{
 		// $stmt = "SELECT t.tid AS taxonID, t.kingdomName AS kingdom, ts.family, t.sciname,CONCAT_WS(' ', t.unitind1, t.unitname1) AS unitname1, CONCAT_WS(' ', t.unitind2, t.unitname2) AS unitname2, t.unitind3, t.unitname3, t.author, t.rankid, t.source, p.tid AS parentNameUsageID,  p.sciname AS parentNameUsage, p.author AS parentNameAuthor, a.tid AS acceptedNameUsageID, a.sciname AS acceptedNameUsage, a.author AS acceptedNameAuthor FROM taxa t INNER JOIN taxstatus ts ON t.tid = ts.tid INNER JOIN taxaenumtree e ON ts.tid = e.tid AND ts.taxauthid = e.taxauthid INNER JOIN taxa p ON ts.parentTid = p.tid INNER JOIN taxa a ON ts.tidaccepted = a.tid WHERE ts.taxauthid = 1 AND e.parentTid = ? ORDER BY t.rankid, ts.family, sciname;";
-		$stmt = "SELECT t.tid AS taxonID, t.kingdomName AS kingdom, ts.family, t.sciname, t.author, CONCAT_WS(' ', t.unitind1, t.unitname1) AS genus, CONCAT_WS(' ', t.unitind2, t.unitname2) AS specificepithet, t.unitind3 AS taxonrank, t.unitname3 AS infraspecificepithet, t.rankid, tu.rankname, t.source, p.sciname AS parentstr FROM taxa t INNER JOIN taxstatus ts ON t.tid = ts.tid INNER JOIN taxaenumtree e ON ts.tid = e.tid AND ts.taxauthid = e.taxauthid INNER JOIN taxa p ON ts.parentTid = p.tid INNER JOIN taxa a ON ts.tidaccepted = a.tid INNER JOIN taxonunits tu ON t.rankid = tu.rankid WHERE ts.taxauthid = 1 AND e.parentTid = ? GROUP BY t.tid ORDER BY t.rankid, ts.family, sciname;";
+		$stmt = "SELECT t.tid AS taxonID, t.kingdomName AS kingdom, ts.family, t.sciname, t.author, CONCAT_WS(' ', t.unitind1, t.unitname1) AS genus, CONCAT_WS(' ', t.unitind2, t.unitname2) AS specificepithet, t.unitind3 AS taxonrank, t.unitname3 AS infraspecificepithet, t.rankid, tu.rankname, t.source, p.sciname AS parentstr FROM taxa t INNER JOIN taxstatus ts ON t.tid = ts.tid INNER JOIN taxaenumtree e ON ts.tid = e.tid AND ts.taxauthid = e.taxauthid INNER JOIN taxa p ON ts.parentTid = p.tid INNER JOIN taxa a ON ts.tidaccepted = a.tid INNER JOIN taxonunits tu ON t.rankid = tu.rankid WHERE ts.taxauthid = 1 AND e.parentTid = ? AND t.rankid BETWEEN ? AND ? GROUP BY t.tid ORDER BY t.rankid, ts.family, sciname;";
 
 		$stmt = $this->conn->prepare($stmt);
-		$stmt->bind_param('i', $node);
+		$stmt->bind_param('iii', $node, $minRankId, $maxRankId);
 		$stmt->execute();
 		// $stmt->bind_result($tid, $kingdomName, $family, $sciname, $unitname1, $unitname2, $unitind3, $unitname3, $author, $rankid, $source, $parentNameUsageID, $parentNameUsage, $parentNameAuthor, $acceptedNameUsageID, $acceptedNameUsage, $acceptedNameAuthor);
 		$stmt->bind_result($tid, $kingdom, $family, $sciname, $author, $genus, $specificepithet, $taxonrank, $infraspecificepithet, $rankid, $rankname, $source, $parentstr);
@@ -110,24 +136,49 @@ class TaxonomyExporter extends Manager
 			$nodeChildrenArr[] = array("taxonID" => $tid, "kingdom" => $kingdom, "family" => $family, "sciname" => $sciname, "author" => $author, "genus" => $genus, "specificepithet" => $specificepithet, "taxonrank" => $taxonrank, "infraspecificepithet" => $infraspecificepithet, "rankid" => $rankid, "rankname" => $rankname, "source" => $source, "parentstr" => $parentstr);
 		}
 		$stmt->close();
+		// remove from array the first element (the root node)
+		array_shift($nodeChildrenArr);
 		return $nodeChildrenArr;
 	}
 
-	/** Filters a tree array by rankid range. Returns a new array with only the nodes that have a rankid within the range.
-	 * @param $treeArr - the tree array to be filtered
-	 * @param $minRank - the minimum rankid to be included in the filtered array
-	 * @param $maxRank - the maximum rankid to be included in the filtered array
-	 * @return array - the filtered array
+	/**
+	 * Get min and max ranges possible in a portal
+	 * Based on `taxonunits` table
+	 *
+	 * @return array - the min and max rankids
 	 */
-	function filterTreeByRank($treeArr, $minRank, $maxRank)
+	function getRanksRange()
 	{
-		$filteredTreeArr = array();
-		foreach ($treeArr as $node) {
-			if ($node['rankid'] >= $minRank && $node['rankid'] <= $maxRank) {
-				$filteredTreeArr[] = $node;
-			}
+		$stmt = "SELECT min(rankid), max(rankid) FROM taxonunits;";
+		$stmt = $this->conn->prepare($stmt);
+		$stmt->execute();
+		$stmt->bind_result($minRank, $maxRank);
+		$rankRange = array();
+		while ($stmt->fetch()) {
+			$rankRange = array("minRank" => $minRank, "maxRank" => $maxRank);
 		}
-		return $filteredTreeArr;
+		$stmt->close();
+		return $rankRange;
+	}
+
+	/**
+	 * Get the rankid of a rankname
+	 * @param $rankname - the rankname to get the rankid of
+	 * @return array - the rankid(s) of the rankname
+	 */
+	function getRankId($rankname)
+	{
+		$stmt = "SELECT DISTINCT rankid FROM taxonunits WHERE rankname = ?;";
+		$stmt = $this->conn->prepare($stmt);
+		$stmt->bind_param('s', $rankname);
+		$stmt->execute();
+		$stmt->bind_result($rankid);
+		$rankidArr = array();
+		while ($stmt->fetch()) {
+			$rankidArr[] = $rankid;
+		}
+		$stmt->close();
+		return $rankidArr;
 	}
 
 	/**
@@ -149,7 +200,6 @@ class TaxonomyExporter extends Manager
 						if ($node['rankname'] == 'Family') {
 							$node['family'] = $node['sciname'];
 						}
-						// if rankid is below 180, make "genus" = NULL
 						if ($node['rankid'] < 180) {
 							$node['genus'] = NULL;
 						}
@@ -160,7 +210,6 @@ class TaxonomyExporter extends Manager
 		}
 		return $conformedTree;
 	}
-
 
 
 	/** Writes a csv file
--- taxa/taxonomy/taxonomyexporterhandler.php
+++ taxa/taxonomy/taxonomyexporterhandler.php
@@ -4,18 +4,6 @@ include_once($SERVER_ROOT . '/classes/TaxonomyExporter.php');
 
 $taxonExp = new TaxonomyExporter();
 
-if (array_key_exists("node", $_REQUEST)) {
-	$node = filter_var($_REQUEST["node"], FILTER_SANITIZE_NUMBER_INT);
-} else {
-	$node = 0;
-}
-
-$rootNode = $taxonExp->setRootNode($node);
-
-$rootNodeData = $taxonExp->getNode($rootNode["tid"]);
-$children = $taxonExp->getNodeChildren($rootNode["tid"]);
-$tree = array_merge($rootNodeData, $children);
-
 // Check for older versions of Symbiota that don't have the $TEMP_DIR_ROOT variable
 if (empty($TEMP_DIR_ROOT)) {
 	$TEMP_DIR_ROOT = $SERVER_ROOT . '/temp';
@@ -23,50 +11,125 @@ if (empty($TEMP_DIR_ROOT)) {
 
 $taxFileName = $TEMP_DIR_ROOT . "/downloads/";
 
-// Writes full Symbiota taxonomy to a single csv file
-$symbtree = $taxonExp->conformTree($tree, "symbiota");
-
-// print_r($symbtree);
-
-if (!empty($symbtree)) {
-
-	$taxFileName .= date("Y-m-d") . "_";
-
-	if ($node == 0) {
-		$DEFAULT_TITLE = str_replace(" ", "_", $DEFAULT_TITLE);
-		$taxFileName .= $DEFAULT_TITLE . "_full_";
-	} else {
-		$DEFAULT_TITLE = str_replace(" ", "_", $DEFAULT_TITLE);
-		$rootNode["sciname"] = str_replace(" ", "_", $rootNode["sciname"]);
-		$taxFileName .= $DEFAULT_TITLE . "_" . $rootNode["sciname"] . "_";
-	}
-
-	$taxFileName .= "taxonomy.csv";
-
-	$taxonExp->writeCsv($symbtree, $taxFileName);
-
-	// Download $symbcsv file if it exists
-
-	if (file_exists($taxFileName)) {
-		header('Content-Description: File Transfer');
-		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename="' . basename($taxFileName) . '"');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate');
-		header('Pragma: public');
-		header('Content-Length: ' . filesize($taxFileName));
-		readfile($taxFileName);
-		exit;
-	} else {
-		echo "There was an error downloading the file.";
-	}
-
-	// Filter tree to include only higher taxa (rankid between 1 and 160)
-
-	// // Add file to zip file
-	// $zipFileName = $TEMP_DIR_ROOT . "/downloads/" . date("Y-m-d") . "_" . $DEFAULT_TITLE . "_taxonomy.zip";
-	// $zip = new ZipArchive();
-	// $zip->open($zipFileName, ZipArchive::CREATE);
-	// $zip->addFile($taxFileName, basename($taxFileName));
-	// $zip->close();
+if (array_key_exists("node", $_REQUEST)) {
+	$node = filter_var($_REQUEST["node"], FILTER_SANITIZE_NUMBER_INT);
+} else {
+	$node = 0;
 }
+
+$rootNodes = $taxonExp->setRootNode($node);
+echo ("<h2>Root Nodes</h2>");
+echo "<pre>" . print_r($rootNodes, true) . "</pre>";
+echo ("<br><br>");
+echo ("<hr>");
+echo ("<hr>");
+
+/****************************************/
+/** Use below to traverse tree node per node */
+// foreach ($rootNodes as $rootNode) {
+// 	$firstChildren = $taxonExp->getNextChildren($rootNode["tid"], $rootNode["rankid"]);
+// 	print_r($firstChildren);
+// 	echo ("<br><br>");
+
+// 	$children = $firstChildren;
+// 	while (!empty($children)) {
+// 		$children = $taxonExp->getNextChildren($rootNode["tid"], $children[0]["rankid"]);
+// 		print_r($children);
+// 		echo ("<br><br>");
+// 	}
+// }
+/****************************************/
+
+/****************************************/
+/** Use below to get single query with all nodes */
+// $ranksRange = $taxonExp->getRanksRange();
+// echo ("<h2>Ranks Range</h2>");
+// echo "<pre>" . print_r($ranksRange, true) . "</pre>";
+
+$kingdomRankId = $taxonExp->getRankId("kingdom");
+$kingdomRankId = $kingdomRankId[0];
+echo ("<h2>Kingdom Rank ID " . $kingdomRankId . "</h2>");
+
+
+$rootNodesData = array();
+$kingdomsData = array();
+// get root node data, children and tree for each root node
+foreach ($rootNodes as $rootNode) {
+	$rootNodeData = $taxonExp->getNode($rootNode["tid"]);
+	// echo ("<h2>Root Node: " . $rootNode["sciname"] . "</h2>");
+	// echo "<pre> . print_r($rootNodeData) . </pre>";
+	// echo ("<br><br>");
+	// add to $rootNodesData
+	$rootNodesData[] = $rootNodeData;
+
+	$children = $taxonExp->getNodeChildren($rootNode["tid"], 1, $kingdomRankId);
+
+	echo ("<h3>Children:</h3>");
+	echo $children ?  "<pre>" . print_r($children, true) . "</pre>" : "No children found.";
+	echo ("<br><br>");
+	echo ("<hr>");
+	$kingdomsData[] = $children;
+}
+
+// echo "<h2>Root Nodes Data:</h2>";
+// echo "<pre>" . print_r($rootNodesData, true) . "</pre>";
+// echo "<h3>Children Data (All Children):</h3>";
+// echo "<pre>" . print_r($childrenData, true) . "</pre>";
+
+// 1. Create CSV with higher taxa (Organisms -> Kingdoms)
+// 2. Create one CSV per kingdom with all the children in that given kingdom
+
+// $tree = array_merge($rootNodeData, $children);
+// echo ("<h2>Full Tree for " . $rootNodeData["rankname"] . " " . $rootNodeData["sciname"] . ":</h2>");
+// echo "<pre>" . print_r($tree, true) . "</pre>";
+// echo ("<hr>");
+// echo ("<br><br>");
+
+/********/
+
+	// Writes full Symbiota taxonomy to a single csv file
+	// $symbtree = $taxonExp->conformTree($tree, "symbiota");
+
+	// print_r($symbtree);
+
+	// if (!empty($symbtree)) {
+
+	// 	$taxFileName .= date("Y-m-d") . "_";
+
+	// 	if ($node == 0) {
+	// 		$DEFAULT_TITLE = str_replace(" ", "_", $DEFAULT_TITLE);
+	// 		$taxFileName .= $DEFAULT_TITLE . "_full_";
+	// 	} else {
+	// 		$DEFAULT_TITLE = str_replace(" ", "_", $DEFAULT_TITLE);
+	// 		$rootNode["sciname"] = str_replace(" ", "_", $rootNode["sciname"]);
+	// 		$taxFileName .= $DEFAULT_TITLE . "_" . $rootNode["sciname"] . "_";
+	// 	}
+
+	// 	$taxFileName .= "taxonomy.csv";
+
+	// 	$taxonExp->writeCsv($symbtree, $taxFileName);
+
+	// 	// Download $symbcsv file if it exists
+
+	// 	if (file_exists($taxFileName)) {
+	// 		header('Content-Description: File Transfer');
+	// 		header('Content-Type: application/octet-stream');
+	// 		header('Content-Disposition: attachment; filename="' . basename($taxFileName) . '"');
+	// 		header('Expires: 0');
+	// 		header('Cache-Control: must-revalidate');
+	// 		header('Pragma: public');
+	// 		header('Content-Length: ' . filesize($taxFileName));
+	// 		readfile($taxFileName);
+	// 		exit;
+	// 	} else {
+	// 		echo "There was an error downloading the file.";
+	// 	}
+
+	// 	// Filter tree to include only higher taxa (rankid between 1 and 160)
+
+	// 	// // Add file to zip file
+	// 	// $zipFileName = $TEMP_DIR_ROOT . "/downloads/" . date("Y-m-d") . "_" . $DEFAULT_TITLE . "_taxonomy.zip";
+	// 	// $zip = new ZipArchive();
+	// 	// $zip->open($zipFileName, ZipArchive::CREATE);
+	// 	// $zip->addFile($taxFileName, basename($taxFileName));
+	// 	// $zip->close();
