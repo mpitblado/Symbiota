97d76201eaa05bd9a4321f2bc0e0227b5e3cff82
Mark <mark.aaron.fisher@gmail.com>
2023-07-12 15:32:38 -0700

    populate links in footer with their counterparts on usda.gov

--- includes/footer.php
+++ includes/footer.php
@@ -17,22 +17,30 @@
                     usa-footer__primary-content--collapsible
                   "
                 >
-                  <h4 class="usa-footer__primary-link">&lt;Topic&gt;</h4>
+                  <h4 class="usa-footer__primary-link">USDA Home</h4>
                   <ul class="usa-list usa-list--unstyled">
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://ask.usda.gov/s/?utm_source=USDA&utm_medium=homepage&utm_campaign=homepage%20referrals">AskUSDA</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.farmers.gov/pandemic-assistance">Pandemic Assistance</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);"
-                        >&lt;Secondary link that&#039;s a bit longer than most
-                        of the others&gt;</a
+                      <a href="https://ask.usda.gov/s/contactsupport"
+                        >Contact Us</a
                       >
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.usda.gov/sitemap">USDA Site Map</a>
+                    </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="https://www.usda.gov/policies-and-links">Policies and Links</a>
+                    </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="https://www.usda.gov/our-agency/about-usda/performance">Our Performance</a>
+                    </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="https://www.usda.gov/our-agency/careers">Careers</a>
                     </li>
                   </ul>
                 </section>
@@ -44,21 +52,30 @@
                     usa-footer__primary-content--collapsible
                   "
                 >
-                  <h4 class="usa-footer__primary-link">&lt;Topic&gt;</h4>
+                  <h4 class="usa-footer__primary-link">Topics</h4>
                   <ul class="usa-list usa-list--unstyled">
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://usdaoig.oversight.gov/resources/contractor-fraud">Report Fraud on USDA Contracts</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);"
-                        >&lt;Secondary link that&#039;s pretty long&gt;</a
+                      <a href="https://usdaoig.oversight.gov/"
+                        >Inspector General</a
                       >
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.usda.gov/plain-writing">Plain Writing</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.usda.gov/open">Open</a>
+                    </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="https://www.usda.gov/ogc/office-information-affairs/foia-division">FOIA</a>
+                    </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="https://www.usda.gov/accessibility-statement">Accessibility Statement</a>
+                    </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="https://www.usda.gov/privacy-policy">Privacy Policy</a>
                     </li>
                   </ul>
                 </section>
@@ -70,19 +87,28 @@
                     usa-footer__primary-content--collapsible
                   "
                 >
-                  <h4 class="usa-footer__primary-link">&lt;Topic&gt;</h4>
+                  <h4 class="usa-footer__primary-link">Our Agency</h4>
                   <ul class="usa-list usa-list--unstyled">
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.usda.gov/non-discrimination-statement">Non-Discrimination Statement</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.usda.gov/oascr/civil-rights-statements">Anti-Harassment Policy</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.usda.gov/#:~:text=Anti%2DHarassment%20Policy-,Information%20Quality,-USA.gov">Information Quality</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.usa.gov/">USA.gov</a>
+                    </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="https://www.whitehouse.gov/">WhiteHouse.gov</a>
+                    </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="https://www.usda.gov/federal-web-site-inventory">eGov</a>
+                    </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="mailto:feedback@oc.usda.gov">Feedback</a>
                     </li>
                   </ul>
                 </section>
@@ -94,20 +120,39 @@
                     usa-footer__primary-content--collapsible
                   "
                 >
-                  <h4 class="usa-footer__primary-link">&lt;Topic&gt;</h4>
+                  <h4 class="usa-footer__primary-link">Priorities</h4>
                   <ul class="usa-list usa-list--unstyled">
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.usda.gov/nofear">No FEAR Act Data</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://osc.gov/">Office of Special Counsel</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://usdaoig.oversight.gov/resources/whistleblower-protection-coordinator">Whistleblower Protection Coordinator</a>
                     </li>
                     <li class="usa-footer__secondary-link">
-                      <a href="javascript:void(0);">&lt;Secondary link&gt;</a>
+                      <a href="https://www.usda.gov/vulnerability-disclosure-policy">Vulnerability Disclosure Poilcy</a>
                     </li>
+                    <li class="usa-footer__secondary-link">
+                      <a href="https://www.usda.gov/ocfo/plans-reports/agency-financial-reports">Agency Financial Reports</a>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+              <div class="mobile-lg:grid-col-6 desktop:grid-col-3">
+                <section
+                  class="
+                    usa-footer__primary-content
+                    usa-footer__primary-content--collapsible
+                  "
+                >
+                  <h4 class="usa-footer__primary-link">Media</h4>
+                  <ul class="usa-list usa-list--unstyled">
+                    <div class="usa-sign_up-block usa-width-one-third">
+                      <h3 class="usa-sign_up-header">Sign Up for Updates</h3>
+                      <a style="color:white;font-weight:bold;" class="usa-button usa-button ext" href="https://public.govdelivery.com/accounts/USDAOCIO/signup/37170" target="_blank" id="anch_114" data-extlink="">Subscribe<svg focusable="false" class="ext" role="img" aria-label="(link is external)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 40"><metadata><sfw xmlns="http://ns.adobe.com/SaveForWeb/1.0/"><slicesourcebounds y="-8160" x="-8165" width="16389" height="16384" bottomleftorigin="true"></slicesourcebounds><optimizationsettings><targetsettings targetsettingsid="0" fileformat="PNG24Format"><png24format transparency="true" filtered="false" interlaced="false" nomattecolor="false" mattecolor="#FFFFFF"></png24format></targetsettings></optimizationsettings></sfw></metadata><title>(link is external)</title><path d="M48 26c-1.1 0-2 0.9-2 2v26H10V18h26c1.1 0 2-0.9 2-2s-0.9-2-2-2H8c-1.1 0-2 0.9-2 2v40c0 1.1 0.9 2 2 2h40c1.1 0 2-0.9 2-2V28C50 26.9 49.1 26 48 26z"></path><path d="M56 6H44c-1.1 0-2 0.9-2 2s0.9 2 2 2h7.2L30.6 30.6c-0.8 0.8-0.8 2 0 2.8C31 33.8 31.5 34 32 34s1-0.2 1.4-0.6L54 12.8V20c0 1.1 0.9 2 2 2s2-0.9 2-2V8C58 6.9 57.1 6 56 6z"></path></svg></a>
+                    </div>
                   </ul>
                 </section>
               </div>
@@ -131,7 +176,7 @@
             <img class="usa-footer__logo-img" src="/assets/img/logo-img.png" alt="" />
           </div>
           <div class="mobile-lg:grid-col-auto">
-            <p class="usa-footer__logo-heading">&lt;Name of Agency&gt;</p>
+            <p class="usa-footer__logo-heading">Symbiota</p>
           </div>
         </div>
         <div class="usa-footer__contact-links mobile-lg:grid-col-6">
