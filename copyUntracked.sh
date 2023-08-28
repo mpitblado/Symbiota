38e4bdb1aa0d152a85218d732ef8ffade360a556
Mark <mark.aaron.fisher@gmail.com>
2023-08-28 14:24:14 -0700

    untrack copyUntracked.sh

--- copyUntracked.sh
+++
@@ -1,4 +0,0 @@
-cp includes/header_template.php includes/header.php
-cp includes/footer_template.php includes/footer.php
-cp includes/head_template.php includes/head.php
-cp includes/usagepolicy_template.php includes/usagepolicy.php
