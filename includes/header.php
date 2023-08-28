6eb1e4f290c6a2e9ef605a618888c22120c01d93
Mark <mark.aaron.fisher@gmail.com>
2023-08-28 16:16:13 -0700

    add logo and begin the work of getting the header to work

--- includes/header.php
+++ includes/header.php
@@ -5,79 +5,149 @@ include_once($SERVER_ROOT.'/classes/ProfileManager.php');
 $pHandler = new ProfileManager();
 $isAccessiblePreferred = $pHandler->getAccessibilityPreference($SYMB_UID);
 ?>
-<div class="header-wrapper">
-  <header>
-    <div class="top-wrapper">
-      <nav class="top-login usda-nav">
-        <span>
-          <a href="#"> <?php echo htmlspecialchars($LANG['CONTACT_US'], HTML_SPECIAL_CHARS_FLAGS) ?> </a>
-        </span>
-        <span class="button button-secondary">
-          <a
-            href="/usda/portal/profile/index.php?refurl=/usda/portal/index.php?"
-          >
-            <?php echo htmlspecialchars($LANG['LOGIN'], HTML_SPECIAL_CHARS_FLAGS) ?>
-          </a>
-        </span>
-      </nav>
-      <div class="top-brand">
-        <a href="https://symbiota.org">
-          <img
-            src="/usda/portal/images/layout/usda-symbol.svg"
-            alt="USDA logo"
-          />
-        </a>
-        <div class="brand-name">
-          <h1>USDA Collections Portal</h1>
-          <h2>Construction in Progress</h2>
-        </div>
+<div class="usa-overlay"></div>
+<header class="usa-header usa-header--basic usa-header--megamenu">
+  <div class="usa-nav-container">
+    <div class="usa-navbar">
+      <div class="usa-logo">
+        <!-- <div style="display: flex"> -->
+        <img src="<?php echo $CLIENT_ROOT ?>/assets/uswds/img/ars-color-lockup.png"/>
+        <em class="usa-logo__text">
+          <a href="/" title="<Project title>">
+              USDA Biocollections Portal
+            </a>
+          </em>
+        <!-- </div> -->
       </div>
+      <button type="button" class="usa-menu-btn">Menu</button>
     </div>
-    <div class="menu-wrapper">
-      <!-- Hamburger icon -->
-      <input class="side-menu" type="checkbox" id="side-menu" />
-      <label class="hamb" for="side-menu"
-        ><span class="hamb-line"></span
-      ></label>
-      <!-- Menu -->
-      <nav class="top-menu usda-nav">
-        <ul class="menu">
-          <li><a href="/usda/portal/index.php"><?php echo htmlspecialchars($LANG['HOME'], HTML_SPECIAL_CHARS_FLAGS) ?></a></li>
-          <li>
-            <a href="/usda/portal/collections/index.php"><?php echo htmlspecialchars($LANG['SEARCH_COLLECTIONS'], HTML_SPECIAL_CHARS_FLAGS) ?></a>
-          </li>
-          <li>
-            <a
-              href="/usda/portal/collections/map/index.php"
-              target="_blank"
-              rel="noopener noreferrer"
-              ><?php echo htmlspecialchars($LANG['H_MAP_SEARCH'], HTML_SPECIAL_CHARS_FLAGS) ?></a
-            >
-          </li>
-          <li><a href="/usda/portal/checklists/index.php"><?php echo htmlspecialchars($LANG['CHECKLISTS'], HTML_SPECIAL_CHARS_FLAGS) ?></a></li>
-          <li><a href="/usda/portal/imagelib/search.php"><?php echo htmlspecialchars($LANG['H_IMAGE_SEARCH'], HTML_SPECIAL_CHARS_FLAGS) ?></a></li>
-          <li><a href="/usda/portal/includes/usagepolicy.php"><?php echo htmlspecialchars($LANG['DATA_USE'], HTML_SPECIAL_CHARS_FLAGS) ?></a></li>
-          <li>
-            <a
-              href="https://symbiota.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              ><?php echo htmlspecialchars($LANG['ABOUT_SYMBIOTA'], HTML_SPECIAL_CHARS_FLAGS) ?></a
-            >
-          </li>
-          <li>
-            <a
-              href="https://symbiota.org/docs"
-              target="_blank"
-              rel="noopener noreferrer"
-              ><?php echo htmlspecialchars($LANG['HELP'], HTML_SPECIAL_CHARS_FLAGS) ?></a
-            >
-          </li>
-          <li><a href="/usda/portal/sitemap.php"><?php echo htmlspecialchars($LANG['H_SITEMAP'], HTML_SPECIAL_CHARS_FLAGS) ?></a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-</div>
+    <nav aria-label="Primary navigation" class="usa-nav">
+      <button type="button" class="usa-nav__close">
+        <img src="/assets/img/usa-icons/close.svg" role="img" alt="Close" />
+      </button>
+      <ul class="usa-nav__primary usa-accordion usa-current">
+        <li class="usa-nav__primary-item">
+          <a href="javascript:void(0);" class="usa-nav-link"
+            >Home</a
+          >
+        </li>
+        <li class="usa-nav__primary-item">
+          <button
+            type="button"
+            class="usa-accordion__button usa-nav__link"
+            aria-expanded="false"
+            aria-controls="basic-mega-nav-section-one"
+          >
+            <span>Search Collections</span>
+          </button>
+          <div
+            id="basic-mega-nav-section-one"
+            class="usa-nav__submenu usa-megamenu"
+          >
+            <div class="grid-row grid-gap-4">
+              <div class="usa-col">
+                <ul class="usa-nav__submenu-list">
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">Search All Collections</a>
+                  </li>
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">Search National Arboretum Herbarium</a>
+                  </li>
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">Search National Seed Herbarium</a>
+                  </li>
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">Search National Fungus Collections</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="usa-nav__primary-item">
+          <button
+            type="button"
+            class="usa-accordion__button usa-nav__link usa-current"
+            aria-expanded="false"
+            aria-controls="basic-mega-nav-section-one"
+          >
+            <span>Map Search</span>
+          </button>
+          <div
+            id="basic-mega-nav-section-one"
+            class="usa-nav__submenu usa-megamenu"
+          >
+            <div class="grid-row grid-gap-4">
+              <div class="usa-col">
+                <ul class="usa-nav__submenu-list">
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">Search All Collections</a>
+                  </li>
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">Search National Arboretum Herbarium</a>
+                  </li>
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">Search National Seed Herbarium</a>
+                  </li>
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">Search National Fungus Collections</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="usa-nav__primary-item">
+          <button
+            type="button"
+            class="usa-accordion__button usa-nav__link"
+            aria-expanded="false"
+            aria-controls="basic-mega-nav-section-one"
+          >
+            <span>About Collections</span>
+          </button>
+          <div
+            id="basic-mega-nav-section-one"
+            class="usa-nav__submenu usa-megamenu"
+          >
+            <div class="grid-row grid-gap-4">
+              <div class="usa-col">
+                <ul class="usa-nav__submenu-list">
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">About All USDA Biocollections</a>
+                  </li>
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">About National Arboretum Herbarium</a>
+                  </li>
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">About National Seed Herbarium</a>
+                  </li>
+                  <li class="usa-nav__submenu-item">
+                    <a href="javascript:void(0);">About National Fungus Collections</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="usa-nav__primary-item">
+          <a href="javascript:void(0);" class="usa-nav-link"
+            >Data Use</a
+          >
+        </li>
+        <li class="usa-nav__primary-item">
+          <a href="javascript:void(0);" class="usa-nav-link"
+            >Help</a
+          >
+        </li>
+        <li class="usa-nav__primary-item">
+          <a href="javascript:void(0);" class="usa-nav-link"
+            >Sitemap</a
+          >
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
 <div class="navpath">
 </div>
