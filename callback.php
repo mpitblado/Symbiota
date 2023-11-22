060f9ba0cfc9fa1b54c97bec055e950fc48a7190
Greg Post <gregory.post@asu.edu>
2023-11-22 12:28:28 -0600

    initial Commit
    setting up temporary hardcoded link to OpenIDConnect class and test / proff of concept examples for Azure Entra

---
+++ callback.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+include_once($SERVER_ROOT.'/vendor/jumbojett/src/OpenIDConnect.php');
+
+use Jumbojett\OpenIDConnectClient;
+
+$oidc = new OpenIDConnectClient($_SESSION['OID_ProviderURL'],
+                                $_SESSION['OID_clientID'],
+                                $_SESSION['OID_clientSecret'],
+				$_SESSION['oidIssuer']);
+
+if (array_key_exists('code', $_REQUEST) && $_REQUEST['code']) {
+  if($oidc->authenticate()){
+    echo '<html><pre>';
+    print_r($oidc->requestUserInfo('given_name'));
+    //echo $user_email;
+    //echo "Welcome in email: $user_email";
+    //print_r($oidc);
+    echo '</pre></html>';
+  }
+}
+
+else {
+  $arr = get_defined_vars();
+  echo '<html><pre>';
+  print_r($arr);
+  echo '</pre></html>';
+}
---
+++ client_example.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+include_once($SERVER_ROOT.'/vendor/jumbojett/src/OpenIDConnect.php');
+
+use Jumbojett\OpenIDConnectClient;
+
+$_SESSION['OID_ProviderURL'] = 'https://login.microsoftonline.com/f41b58ac-de75-4b41-986c-17eaa32822e9/v2.0/';
+$_SESSION['OID_clientID'] = 'f4159842-0bca-4297-a860-ab3c9a997056';
+$_SESSION['OID_clientSecret'] = 'WLr8Q~nlBr-v1XVEUgL3cWQG4BWLpbMFW.Omebs7';
+
+$oidc = new OpenIDConnectClient($_SESSION['OID_ProviderURL'],
+                                $_SESSION['OID_clientID'],
+                                $_SESSION['OID_clientSecret']);
+
+$oidc->addScope(array('openid'));
+$oidc->addScope(array('email'));
+$oidc->setResponseTypes(array('code'));
+//$oidc->setResponseTypes(array('id_token'));
+$oidc->setRedirectUrl('https://dev002.symbiota.org/callback.php');
+
+$_SESSION['oidIssuer'] = $oidc->getIssuer();
+$oidc->authenticate();
+
+//$name = $oidc->requestUserInfo('given_name');
+
+
+?>
+
+<html>
+<head>
+    <title>Example OpenID Connect Client Use</title>
+    <style>
+        body {
+            font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+
+    <div>
+        Hello <?php echo $name; ?>
+    </div>
+
+</body>
+</html>
+
