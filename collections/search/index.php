9987e728861a47caa8ed5ecdc0669975e90a1dab
Mark <mark.aaron.fisher@gmail.com>
2023-12-26 20:17:19 -0800

    scrub references to v202209 and replace with uswds within collections/search (#800)

--- collections/search/index.php
+++ collections/search/index.php
@@ -39,7 +39,7 @@ $obsArr = (isset($collList['obs'])?$collList['obs']:null);
 	echo '<link href="' . $CLIENT_ROOT . '/collections/search/css/app.css" type="text/css" rel="stylesheet" />';
 	
 	echo '<link href="' . $CLIENT_ROOT . '/collections/search/css/tables.css" type="text/css" rel="stylesheet" />';
-	echo '<link href="' . $CLIENT_ROOT . '/css/v202209/symbiota/collections/sharedCollectionStyling.css" type="text/css" rel="stylesheet" />';
+	echo '<link href="' . $CLIENT_ROOT . '/css/uswds/symbiota/collections/sharedCollectionStyling.css" type="text/css" rel="stylesheet" />';
 	?>
 	<script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
 	<script>
