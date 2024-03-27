13ce718a546a52c6d1bfae279fb77441ae48182d
Mark <mark.aaron.fisher@gmail.com>
2024-03-26 17:04:36 -0700

    update head.php with head_template

--- includes/head.php
+++ includes/head.php
@@ -24,6 +24,14 @@ $localSession = isset($_SESSION['active_stylesheet']) ? $_SESSION['active_styles
 <script src="<?php echo htmlspecialchars($CLIENT_ROOT, HTML_SPECIAL_CHARS_FLAGS); ?>/assets/uswds/js/uswds-init.min.js" type="text/javascript"></script>
 <link href="<?php echo htmlspecialchars($CSS_BASE_PATH, HTML_SPECIAL_CHARS_FLAGS); ?>/symbiota/styleguide.css" type="text/css" rel="stylesheet">
 <link href="<?php echo htmlspecialchars($CSS_BASE_PATH, HTML_SPECIAL_CHARS_FLAGS); ?>/symbiota/nal.js" type="text/css" rel="stylesheet">
+<style>
+    .accessibility-button {
+        font-size: 1.1em;
+    }
+    .welcome-text {
+        margin-bottom: 0.75rem;
+    }
+</style>
 <?php 
     if($isAccessiblePreferred){
         if(isset($localSession) && strpos($localSession, "condensed.css")){
