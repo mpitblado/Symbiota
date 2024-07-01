94ec500dcb9c88a4fdf291ab4105ce8cca725062
Mark <mark.aaron.fisher@gmail.com>
2024-07-01 12:44:25 -0700

    DRY up taxa suggest in a way that is usable for more than one element on the same page

--- js/symb/api.taxonomy.taxasuggest.js
+++ js/symb/api.taxonomy.taxasuggest.js
@@ -25,133 +25,74 @@ function initTaxaSuggest() {
     return term.split(/,\s*/).pop();
   }
 
-  $("#taxa")
-    // don't navigate away from the field on tab when selecting an item
-    .bind("keydown", function (event) {
-      // don't honor ENTER key if an autocomplete is not selected yet
-      /*
+  function runGenericTaxaSuggest(inputId, taxonTypeId) {
+    $("#" + inputId)
+      // don't navigate away from the field on tab when selecting an item
+      .bind("keydown", function (event) {
+        // don't honor ENTER key if an autocomplete is not selected yet
+        /*
 			if (event.keyCode === $.ui.keyCode.ENTER) {
 				if (this.autocomplete_stage != 0) {
 				    event.preventDefault();
 				}
 		    } else
 		    */
-      // don't navigate away from the field on tab when selecting an item
-      if (event.keyCode === $.ui.keyCode.TAB) {
-        if ($(this).autocomplete("widget").is(":visible")) {
-          $(this).trigger("select");
-          event.preventDefault();
-        }
-      } else {
-        this.autocomplete_stage = 1;
-      }
-    })
-    .autocomplete(
-      {
-        source: $.proxy(function (request, response) {
-          $.getJSON(
-            acUrl,
-            {
-              term: extractLast(request.term),
-              t: function () {
-                return $("#taxontype").val();
-              },
-            },
-            response
-          );
-          this.autocomplete_stage = 0;
-        }, $("#taxa")[0]),
-        autoFocus: true,
-        search: function () {
-          // custom minLength
-          this.autocomplete_stage = 2;
-          var term = extractLast(this.value);
-          if (term.length < 4) {
-            return false;
+        // don't navigate away from the field on tab when selecting an item
+        if (event.keyCode === $.ui.keyCode.TAB) {
+          if ($(this).autocomplete("widget").is(":visible")) {
+            $(this).trigger("select");
+            event.preventDefault();
           }
-          this.autocomplete_stage = 3;
-          return true;
+        } else {
+          this.autocomplete_stage = 1;
+        }
+      })
+      .autocomplete(
+        {
+          source: $.proxy(function (request, response) {
+            $.getJSON(
+              acUrl,
+              {
+                term: extractLast(request.term),
+                t: function () {
+                  return $("#" + taxonTypeId).val();
+                },
+              },
+              response
+            );
+            this.autocomplete_stage = 0;
+          }, $("#" + inputId)[0]),
+          autoFocus: true,
+          search: function () {
+            // custom minLength
+            this.autocomplete_stage = 2;
+            var term = extractLast(this.value);
+            if (term.length < 4) {
+              return false;
+            }
+            this.autocomplete_stage = 3;
+            return true;
+          },
+          focus: function () {
+            // prevent value inserted on focus
+            return false;
+          },
+          select: function (event, ui) {
+            var terms = this.value.split(/,\s*/);
+            // remove the current input
+            terms.pop();
+            // add the selected item
+            terms.push(ui.item.value);
+            this.value = terms.join(", ");
+            return false;
+          },
         },
-        focus: function () {
-          // prevent value inserted on focus
-          return false;
-        },
-        select: function (event, ui) {
-          var terms = this.value.split(/,\s*/);
-          // remove the current input
-          terms.pop();
-          // add the selected item
-          terms.push(ui.item.value);
-          this.value = terms.join(", ");
-          return false;
-        },
-      },
-      {}
-    );
+        {}
+      );
+  }
 
-  $("#associated-taxa")
-    // don't navigate away from the field on tab when selecting an item
-    .bind("keydown", function (event) {
-      // don't honor ENTER key if an autocomplete is not selected yet
-      /*
-			if (event.keyCode === $.ui.keyCode.ENTER) {
-				if (this.autocomplete_stage != 0) {
-				    event.preventDefault();
-				}
-		    } else
-		    */
-      // don't navigate away from the field on tab when selecting an item
-      if (event.keyCode === $.ui.keyCode.TAB) {
-        if ($(this).autocomplete("widget").is(":visible")) {
-          $(this).trigger("select");
-          event.preventDefault();
-        }
-      } else {
-        this.autocomplete_stage = 1;
-      }
-    })
-    .autocomplete(
-      {
-        source: $.proxy(function (request, response) {
-          $.getJSON(
-            acUrl,
-            {
-              term: extractLast(request.term),
-              t: function () {
-                return $("#taxontype-2").val();
-              },
-            },
-            response
-          );
-          this.autocomplete_stage = 0;
-        }, $("#associated-taxa")[0]),
-        autoFocus: true,
-        search: function () {
-          // custom minLength
-          this.autocomplete_stage = 2;
-          var term = extractLast(this.value);
-          if (term.length < 4) {
-            return false;
-          }
-          this.autocomplete_stage = 3;
-          return true;
-        },
-        focus: function () {
-          // prevent value inserted on focus
-          return false;
-        },
-        select: function (event, ui) {
-          var terms = this.value.split(/,\s*/);
-          // remove the current input
-          terms.pop();
-          // add the selected item
-          terms.push(ui.item.value);
-          this.value = terms.join(", ");
-          return false;
-        },
-      },
-      {}
-    );
+  runGenericTaxaSuggest("taxa", "taxontype");
+  runGenericTaxaSuggest("associated-taxa", "taxontype-2");
 }
 
 $(document).ready(initTaxaSuggest);
