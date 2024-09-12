cc23b29c9ccc77cbde6c359e64d46d2c944cbe4f
Greg Post <gregory.post@asu.edu>
2024-09-11 22:58:46 -0500

    implement Federal DAP tag
    Requires adding new variable $FEDERAL_DAP_AGENCY to symbini

--- includes/googleanalytics.php
+++ includes/googleanalytics.php
@@ -1,29 +1,34 @@
 <?php
-include_once ($SERVER_ROOT.'/classes/UtilityFunctions.php');
-if(isset($GOOGLE_ANALYTICS_KEY) && $GOOGLE_ANALYTICS_KEY) {
-	?>
-	<script type="text/javascript">
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		  ga('create', '<?php echo $GOOGLE_ANALYTICS_KEY; ?>', '<?php echo UtilityFunctions::getDomain(); ?>');
-		  ga('send', 'pageview');
-	</script>
-	<?php
+if(isset($FEDERAL_DAP_AGENCY) && $FEDERAL_DAP_AGENCY){
+	include_once ('federalDAP.php');
 }
-if(isset($GOOGLE_ANALYTICS_TAG_ID) && $GOOGLE_ANALYTICS_TAG_ID) {
-	?>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $GOOGLE_ANALYTICS_TAG_ID; ?>"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
+else{
+	include_once ($SERVER_ROOT.'/classes/UtilityFunctions.php');
+	if(isset($GOOGLE_ANALYTICS_KEY) && $GOOGLE_ANALYTICS_KEY) {
+		?>
+		<script type="text/javascript">
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		gtag('config', '<?php echo $GOOGLE_ANALYTICS_TAG_ID; ?>');
-	</script>
-	<?php
+			ga('create', '<?php echo $GOOGLE_ANALYTICS_KEY; ?>', '<?php echo UtilityFunctions::getDomain(); ?>');
+			ga('send', 'pageview');
+		</script>
+		<?php
+	}
+	if(isset($GOOGLE_ANALYTICS_TAG_ID) && $GOOGLE_ANALYTICS_TAG_ID) {
+		?>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $GOOGLE_ANALYTICS_TAG_ID; ?>"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', '<?php echo $GOOGLE_ANALYTICS_TAG_ID; ?>');
+		</script>
+		<?php
+	}
 }
 ?>
