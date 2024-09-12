583bcf1ff6f00842761ef5b5712b09be5dd0ad9a
Greg Post <gregory.post@asu.edu>
2024-09-12 10:29:29 -0500

    modify Federal DAP
    merge Federal DAP code into existing Google Analytics file.

--- includes/federalDAP.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-* https://github.com/digital-analytics-program/gov-wide-code/wiki/DAP-Quick-Technical-Guide#dap-centrally-hosted-url-implementation-instructions
-*/
-?>
-<script async type="text/javascript" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=<?= $FEDERAL_DAP_AGENCY ?>"  id="_fed_an_ua_tag"></script>
--- includes/googleanalytics.php
+++ includes/googleanalytics.php
@@ -1,6 +1,11 @@
 <?php
 if(isset($FEDERAL_DAP_AGENCY) && $FEDERAL_DAP_AGENCY){
-	include_once ('federalDAP.php');
+	/*
+	* https://github.com/digital-analytics-program/gov-wide-code/wiki/DAP-Quick-Technical-Guide#dap-centrally-hosted-url-implementation-instructions
+	*/
+	?>
+	<script async type="text/javascript" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=<?= $FEDERAL_DAP_AGENCY ?>"  id="_fed_an_ua_tag"></script>
+	<?php
 }
 else{
 	include_once ($SERVER_ROOT.'/classes/UtilityFunctions.php');
