32bdb97b9aba81430b6588c0568f5b813526fa1b
MuchQuak <loganpwilt@gmail.com>
2024-09-16 15:23:57 -0700

    imagelib index lang updates

--- content/lang/imagelib/index.en.php
+++ content/lang/imagelib/index.en.php
@@ -2,26 +2,26 @@
 /*
 ------------------
 Language: English
+Translated by: Google Translate (2024-09-16)
 ------------------
 */
 
-$LANG['IMG_LIBRARY'] = 'Image Library';
+$LANG['IMG_LIBRARY'] = 'Multimedia Library';
 $LANG['HOME'] = 'Home';
-$LANG['TAXA_W_IMGS'] = 'Taxa with Images';
-$LANG['TAXA_IMG_EXPLAIN'] = 'This page provides a complete list of taxa that have images.
-		Use the controls below to browse and search for images by family, genus, or species.';
+$LANG['TAXA_W_IMGS'] = 'Taxa with Multimedia';
+$LANG['TAXA_IMG_EXPLAIN'] = 'This page provides a complete list of taxa that have multimedia.                Use the controls below to browse and search for multimedia by family, genus, or species.';
 $LANG['BROWSE_FAMILY'] = 'Browse by Family';
 $LANG['BROWSE_GENUS'] = 'Browse by Genus';
 $LANG['BROWSE_SPECIES'] = 'Browse by Species';
 $LANG['SCINAME_SEARCH'] = 'Scientific Name Search';
 $LANG['ENTER_TAXON_NAME'] = 'Enter family, genus, or scientific name';
 $LANG['SEARCH'] = 'Search';
-$LANG['IMG_CP_POLICY'] = 'Image Copyright Policy';
-$LANG['IMG_CONTRIBUTORS'] = 'Image Contributors';
-$LANG['IMG_SEARCH'] = 'Image Search';
+$LANG['IMG_CP_POLICY'] = 'Multimedia Copyright Policy';
+$LANG['IMG_CONTRIBUTORS'] = 'Multimedia Contributors';
+$LANG['IMG_SEARCH'] = 'Multimedia Search';
 $LANG['SELECT_GENUS'] = 'Select a genus to see species list.';
 $LANG['NO_TAXA_RETURNED'] = 'No taxa returned matching search results';
-$LANG['SELECT_SPECIES'] = 'Select a species to access available images';
+$LANG['SELECT_SPECIES'] = 'Select a species to access available multimedia';
 $LANG['SELECT_FAMILY'] = 'Select a family to see species list';
 
 ?>
