566b04f31127b970203b8a08f9eccff9bec98458
MuchQuak <loganpwilt@gmail.com>
2024-09-16 15:29:03 -0700

    contributors lang updates

--- content/lang/imagelib/contributors.en.php
+++ content/lang/imagelib/contributors.en.php
@@ -2,13 +2,15 @@
 /*
 ------------------
 Language: English
+Translated by: Google Translate (2024-09-16)
 ------------------
 */
 
 $LANG['NAV_HOME'] = 'Home';
-$LANG['NAV_IMG_LIB'] = 'Image Library';
-$LANG['NAV_IMG_CONTR'] = 'Image Contributors';
+$LANG['NAV_IMG_LIB'] = 'Multimedia Library';
+$LANG['NAV_IMG_CONTR'] = 'Multimedia Contributors';
 $LANG['SPECIMENS'] = 'Specimens';
-$LANG['IMG_CONTR'] = 'Image Contributors';
+$LANG['IMG_CONTR'] = 'Multimedia Contributors';
 $LANG['OBS'] = 'Observations';
+
 ?>
--- content/lang/imagelib/contributors.es.php
+++ content/lang/imagelib/contributors.es.php
@@ -2,13 +2,15 @@
 /*
 ------------------
 Language: Español (Spanish)
+Translated by: Google Translate (2024-09-16)
 ------------------
 */
 
 $LANG['NAV_HOME'] = 'Inicio';
-$LANG['NAV_IMG_LIB'] = 'Biblioteca de Imágenes';
-$LANG['NAV_IMG_CONTR'] = 'Colaboradores de Imágenes';
+$LANG['NAV_IMG_LIB'] = 'Biblioteca multimedia';
+$LANG['NAV_IMG_CONTR'] = 'Colaboradores multimedia';
 $LANG['SPECIMENS'] = 'Especímenes';
-$LANG['IMG_CONTR'] = 'Contribuidores de imágenes';
+$LANG['IMG_CONTR'] = 'Colaboradores multimedia';
 $LANG['OBS'] = 'Observaciones';
+
 ?>
