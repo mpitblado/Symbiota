572e5ba11154abeb08f6a39b3d6283e75da21a1e
Mark <mark.aaron.fisher@gmail.com>
2024-09-24 14:21:34 -0700

    create and improve upon splitScinameFromOccArr

--- traits/TaxonomyTrait.php
+++ traits/TaxonomyTrait.php
@@ -28,21 +28,43 @@ trait TaxonomyTrait {
 		$scinameBase = $sciname;
 		if(!empty($tradeName)){
 			$scinameBase = str_replace($tradeName, '', $scinameBase);
-		}else{
-			// @TODO try to split at scientificnameauthorship
+			$scinameBase = str_replace(strtoupper($tradeName), '', $scinameBase);
 		}
 
 		if(!empty($cultivarEpithet)){
 			$scinameBase = str_replace("'". $cultivarEpithet . "'", '', trim($scinameBase)); // @TODO could possibly replace off-target if cultivarEpithet matches some parent taxon exactly. We think extremely unlikely edge case, so ignoring for now.
-		} else{
-			// @TODO try to split at tradeName
+			$scinameBase = str_replace($cultivarEpithet, '', trim($scinameBase));
 		}
+		$splitByAuthorship = explode(ucfirst(strtolower($scientificnameauthorship)), trim($scinameBase));
+		// var_dump($splitByAuthorship);
+		if(count($splitByAuthorship)==2){
+			$scinameBase = trim($splitByAuthorship[0]);
+			// var_dump($secondSplit);
+			$theStuffBeyondAuthor = trim($splitByAuthorship[1]);
+			$secondSplit = preg_split("/(\w+\.)/", $theStuffBeyondAuthor,-1,PREG_SPLIT_DELIM_CAPTURE);
+			$returnObj['nonItal'] = $secondSplit[count($secondSplit)-1];
+			var_dump($secondSplit);
+		}else{
+			// $theStuffBeyondAuthor = trim($splitByAuthorship[1]);
+			$secondSplit = preg_split("/(\w+\.)/", trim($scinameBase), -1, PREG_SPLIT_DELIM_CAPTURE);
+			var_dump($secondSplit);
+			$returnObj['nonItal'] = $secondSplit[count($secondSplit)-1];
+		}
+
+
+		// if(!empty($scientificnameauthorship)){
+		// 	$scinameBase = str_replace($scientificnameauthorship, '', trim($scinameBase)); // @TODO will not work with complex multi-word last names yet
+		// 	$scinameBase = str_replace(strtoupper($scientificnameauthorship), '', trim($scinameBase));
+		// 	$scinameBase = str_replace(ucfirst(strtolower($scientificnameauthorship)), '', trim($scinameBase));
+		// }
+
+
 		$returnObj['base'] = trim($scinameBase ?? '');
 		$returnObj['cultivarEpithet'] = $cultivarEpithet; // @TODO other options if missing; assumes quotes not stored in db
 		$returnObj['tradeName'] = $tradeName;
 		$returnObj['author'] = $scientificnameauthorship;
 
-
+		return $returnObj;
 	}
 
 	function splitScinameByProvided($sciName, $cultivarEpithet = '', $tradeName = '', $author = ''){
