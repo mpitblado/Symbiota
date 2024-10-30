784ac2a0f4ddb85fb5f1d83bbb598aae61ce9d77
Mark <mark.aaron.fisher@gmail.com>
2024-10-30 13:14:49 -0700

    update composer for phpseclib/phpseclib explicitly to ^3.0.42, even though this was already done automatically

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'f65c7fc4357b8c6b769ed7cb2848d3ef67251e3f',
+        'reference' => '7894a4a232c9c12728cdce3472e8ebbbc6c3e13a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'f65c7fc4357b8c6b769ed7cb2848d3ef67251e3f',
+            'reference' => '7894a4a232c9c12728cdce3472e8ebbbc6c3e13a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
