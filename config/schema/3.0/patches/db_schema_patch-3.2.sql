ea8c518f3cad6f66b9bf6ae98b512e8fcf15632d
Edward Gilbert <egbot@asu.edu>
2024-10-31 08:52:38 -0700

    Association schema adjustment
    - Adjust schema to remove association record whenever either internal association is deleted. Current schema sets occidAssociate field to null when linked occurrence is deleted, thus resulting in non-informative orphaned associations.

--- config/schema/3.0/patches/db_schema_patch-3.2.sql
+++ config/schema/3.0/patches/db_schema_patch-3.2.sql
@@ -149,4 +149,12 @@ ALTER TABLE `kmdescr`
   ADD CONSTRAINT `FK_descr_cs`  FOREIGN KEY (`cid` , `cs`)  REFERENCES `kmcs` (`cid` , `cs`)  ON DELETE CASCADE  ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_descr_tid`  FOREIGN KEY (`tid`)  REFERENCES `taxa` (`TID`)  ON DELETE CASCADE  ON UPDATE CASCADE;
 
+#Adjust FK to remove associations on deletion of either internal occurrence 
+ALTER TABLE `omoccurassociations`
+	DROP FOREIGN KEY `FK_occurassoc_occidassoc`;
+
+ALTER TABLE `omoccurassociations`
+	ADD CONSTRAINT `FK_occurassoc_occidassoc` FOREIGN KEY (`occidAssociate`) REFERENCES `omoccurrences` (`occid`) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
 
